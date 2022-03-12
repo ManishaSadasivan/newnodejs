2b4db29cc69fb3d4ab5a4ca27c6c2ed28634f7e9
ManishaSadasivan <54832964+ManishaSadasivan@users.noreply.github.com>
2022-03-12 14:48:34 +0530

    changes

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-// console.log(process.argv);
-// // const [,,a,b]=process.argv;
-// // function sum(a, b) {
-// //     return a+b;
-// // }
-// // const [,,num]=process.argv;
-// // function dupl(num){
-// // return num*num
-// // }
-// // console.log(dupl(num));
-// // console.log(process.argv[2]);
-// // console.log(global);
-// // const os=require('os');
-// // console.log("free storage ",os.freemem());
-// // console.log("free storage ",os.version());
-// // console.log("free storage ",os.cpus());
+console.log(process.argv);
+// const [,,a,b]=process.argv;
+// function sum(a, b) {
+//     return a+b;
+// }
+// const [,,num]=process.argv;
+// function dupl(num){
+// return num*num
+// }
+// console.log(dupl(num));
+// console.log(process.argv[2]);
+// console.log(global);
+// const os=require('os');
+// console.log("free storage ",os.freemem());
+// console.log("free storage ",os.version());
+// console.log("free storage ",os.cpus());
 
-// const fs=require('fs');
-// // const data='hello all';
-// // fs.writeFile('./new.txt',data,function(err){
+const fs=require('fs');
+// const data='hello all';
+// fs.writeFile('./new.txt',data,function(err){
   
-// //     console.log("completed writing");
-
-// // })
-
-// fs.readFile('text.txt','utf-8',function(err,data){
-//     if(err)
-//     {
-//         throw err;
-//     }
-//     else{
-//         console.log(data);
-//     }
+//     console.log("completed writing");
 
 // })
+
+fs.readFile('text.txt','utf-8',function(err,data){
+    if(err)
+    {
+        throw err;
+    }
+    else{
+        console.log(data);
+    }
+
+})
 
 
 var fs= require('fs');
